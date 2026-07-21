71960b5e714635e25289f178eceb8c09dda2cbad
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-07-21 16:15:07 +0200

    Code cleaning

--- DiGi.Analytical.Building.HVAC/Enums/Parameter/InternalCondition.cs
+++ DiGi.Analytical.Building.HVAC/Enums/Parameter/InternalCondition.cs
@@ -1,4 +1,4 @@
-﻿using DiGi.Analytical.Building.Interfaces;
+using DiGi.Analytical.Building.Interfaces;
 using DiGi.Core.Parameter.Classes;
 using System.ComponentModel;
 
@@ -20,7 +20,9 @@ namespace DiGi.Analytical.Building.HVAC.Enums
         /// </summary>
         [ParameterProperties("3686aa6d-3590-4bcf-8382-c2c086f5874b", "Thermostat", "Thermostat"), ObjectParameterValue(true, true, typeof(Classes.Thermostat))] Thermostat,
 
-        /// <summary>Represents the heating emitter internal condition parameter.</summary>
+        /// <summary>
+        /// Represents the heating emitter internal condition parameter.
+        /// </summary>
         [ParameterProperties("1cb95e8b-fb84-4d87-ae53-7949f9235874", "HeatingEmitter", "Heating Emitter"), ObjectParameterValue(true, true, typeof(Classes.HeatingEmitter))] HeatingEmitter,
 
         /// <summary>
@@ -28,7 +30,9 @@ namespace DiGi.Analytical.Building.HVAC.Enums
         /// </summary>
         [ParameterProperties("210d1fa2-66ba-4721-917a-4d52f2689767", "CoolingEmitter", "Cooling Emitter"), ObjectParameterValue(true, true, typeof(Classes.CoolingEmitter))] CoolingEmitter,
 
-        /// <summary>Represents the system properties parameter for the internal condition.</summary>
+        /// <summary>
+        /// Represents the system properties parameter for the internal condition.
+        /// </summary>
         [ParameterProperties("be085040-2032-4008-bb44-b1aa78b1ef32", "SystemProperties", "System Properties"), ObjectParameterValue(true, true, typeof(Classes.SystemProperties))] SystemProperties,
     }
 }
